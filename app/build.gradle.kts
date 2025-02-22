e135203f954e94518e5483480e953f61dfe45610
Edward Fantasia <98419624+EdwardFantasia@users.noreply.github.com>
2025-02-22 15:50:09 -0500

    initial UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Added
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
+    implementation(libs.mlkit.face.detection)
+    implementation(libs.mlkit.segmentation.selfie)
 }
--- app/src/main/java/com/example/project3/MainActivity.kt
+++ app/src/main/java/com/example/project3/MainActivity.kt
@@ -12,36 +12,271 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project3.ui.theme.Project3Theme
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        if(!hasPermissions()){
+            ActivityCompat.requestPermissions(
+                this, CAMERA_PERMISSIONS, 0
+            )
+        }
         setContent {
             Project3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(
+                            CameraController.IMAGE_CAPTURE
+                        )
+                    }
+                }
+
+                var capturedImage = remember{
+                    mutableStateOf<Bitmap?>(null)
+                }
+
+                var (selected, setSelected) = remember { mutableStateOf("") }
+
+                Column(
+                    modifier = Modifier
+                        .padding(PaddingValues()),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(500.dp),
+                        contentAlignment = Alignment.Center
+                    ){
+                        if(capturedImage.value != null){
+                            Image(
+                                bitmap = capturedImage.value!!.asImageBitmap(),
+                                contentDescription = "Captured Photo",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(2.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        }else{
+                            CameraPreview(
+                                controller = controller,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .height(500.dp)
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
                     )
+
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {takePhoto(controller = controller, onPhotoTaken = {bitmap -> capturedImage.value = bitmap} )},
+                        shape = RoundedCornerShape(1.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Blue,
+                            contentColor = Color.White
+                        )
+                    ){
+                        Text("Take a picture", fontSize = 16.sp)
+                    }
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        RadioButton(
+                            selected = ("none" == selected),
+                            onClick = {setSelected("none")}
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "None")
+                    }
+
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        RadioButton(
+                            selected = ("fd" == selected),
+                            onClick = {setSelected("fd")}
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "Face detection")
+                    }
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        RadioButton(
+                            selected = ("cd" == selected),
+                            onClick = {setSelected("cd")}
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "Contour detection")
+                    }
+
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        RadioButton(
+                            selected = ("md" == selected),
+                            onClick = {setSelected("md")}
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "Mesh detection")
+                    }
+
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        RadioButton(
+                            selected = ("ss" == selected),
+                            onClick = {setSelected("ss")}
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "Selfie segmentation")
+                    }
                 }
             }
         }
     }
+
+    private fun takePhoto(
+        controller: LifecycleCameraController,
+        onPhotoTaken: (Bitmap) -> Unit
+    ){
+        controller.takePicture(
+            ContextCompat.getMainExecutor(applicationContext), object : OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    super.onCaptureSuccess(image)
+
+                    val rotationDegrees = image.imageInfo.rotationDegrees
+
+                    val originalBitmap = image.toBitmap()
+
+                    val matrix = android.graphics.Matrix().apply{
+                        postRotate(rotationDegrees.toFloat())
+                    }
+
+                    val rotatedBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, originalBitmap.width, originalBitmap.height, matrix, true)
+
+                    image.close()
+                    onPhotoTaken(rotatedBitmap)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    super.onError(exception)
+                    Log.e("Camera: ", "Couldn't take picture", exception)
+                }
+
+            }
+        )
+    }
+
+    private fun hasPermissions(): Boolean{
+        return CAMERA_PERMISSIONS.all{
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    companion object{
+        private val CAMERA_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA
+        )
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CameraPreview(controller: LifecycleCameraController, modifier: Modifier){
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = {
+            PreviewView(it).apply {
+                this.controller = controller
+                controller.bindToLifecycle(lifecycleOwner)
+            }
+        },
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Project3Theme {
-        Greeting("Android")
-    }
 }
